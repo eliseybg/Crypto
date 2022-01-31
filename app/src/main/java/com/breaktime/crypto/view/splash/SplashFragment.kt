737ce880296b063a5232ef6561352b00dc4104fb
Elisey Budnitsky <bgs6544@gmail.com>
2022-01-31 17:40:24 +0300

    feat: add splash screen

---
+++ app/src/main/java/com/breaktime/crypto/view/splash/SplashFragment.kt
@@ -0,0 +1,18 @@
+package com.breaktime.crypto.view.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.breaktime.crypto.R
+
+class SplashFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+}
